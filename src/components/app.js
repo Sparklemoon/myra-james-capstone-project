5a08d3845a343b259f01efa70ae597dd1bc300d4
Myra James <Treesmellgood@gmail.com>
2020-12-09 20:25:53 -0500

    added BrowserRouter

--- src/components/app.js
+++ src/components/app.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, BrowserRouter } from  "react-router-dom";
 
 import Home from "./home.js";
 import About from "./about.js";
 import Gallery from "./gallery.js";
 import Contact from "./contact.js";
 
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>Parker James Photography</h1>
-        <div className="navbar-wrapper">
-          <Home />
-          <About />
-          <Gallery />
-          <Contact />
-        </div>
+
+          <header>
+            <h1>Parker James Photography</h1>
+            <Link to="/">Home</Link>
+            <Link to="/">Gallery</Link>
+            <Link to="/">About</Link>
+            <Link to="/">Contact</Link>
+          </header>
+        
+          <div className="navbar-routes">
+            <BrowserRouter>
+              <div>
+                  <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route path="/gallery" component={Gallery} />
+                    <Route path="/about" component={About} />
+                    <Route path="/contact" component={Contact} />
+                  </Switch>
+              </div>
+            </BrowserRouter>
+          </div>
+        
       </div>
     );
   }
